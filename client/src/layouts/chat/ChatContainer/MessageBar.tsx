81abfc4379c14f95ea74e69d00cc028592c8742a
Deepak sadhwani <108820079+Deepaksadhwani@users.noreply.github.com>
2024-09-22 16:39:51 +0530

    fixed message bar responsiveness for mobile phone

--- client/src/layouts/chat/ChatContainer/MessageBar.tsx
+++ client/src/layouts/chat/ChatContainer/MessageBar.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { GrAttachment } from "react-icons/gr";
 import { IoSend } from "react-icons/io5";
 import { RiEmojiStickerLine } from "react-icons/ri";
@@ -13,10 +13,10 @@ const MessageBar = () => {
   const socket: any = useSocket();
   const [emojiPickerOpen, setEmojiPickerOpen] = useState(false);
   const selectedChatType = useSelector(
-    (state: RootState) => state.chat.selectedChatType,
+    (state: RootState) => state.chat.selectedChatType
   );
   const selectedChatData = useSelector(
-    (state: RootState) => state.chat.selectedChatData,
+    (state: RootState) => state.chat.selectedChatData
   );
   const userData = useSelector((state: RootState) => state.user.userData);
   const emojiRef = useRef<any>();
@@ -52,12 +52,14 @@ const MessageBar = () => {
     }
     setMessage("");
   };
+
   const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
     if (event.key === "Enter") {
       event.preventDefault();
       handleSendMessage();
     }
   };
+
   useEffect(() => {
     const handleClickOutside = (event: any) => {
       if (emojiRef && !emojiRef.current.contains(event.target)) {
@@ -76,7 +78,7 @@ const MessageBar = () => {
   };
 
   const changeAttachementHandler = async (
-    event: React.ChangeEvent<HTMLInputElement>,
+    event: React.ChangeEvent<HTMLInputElement>
   ) => {
     try {
       const file = event.target.files?.[0];
@@ -111,22 +113,23 @@ const MessageBar = () => {
       console.log({ error });
     }
   };
+
   return (
-    <div className="mb-6 flex h-[10vh] items-center justify-center gap-6 bg-[#1c1d25] px-8">
-      <div className="flex flex-1 items-center gap-5 rounded-md bg-[#2a2b33] pr-5">
+    <div className="mb-2 sm:mb-4 md:mb-6 flex h-auto sm:h-[8vh] md:h-[10vh] items-center justify-center gap-2 sm:gap-4 md:gap-6 bg-[#1c1d25] px-2 sm:px-4 md:px-6">
+      <div className="flex flex-1 items-center gap-2 sm:gap-3 md:gap-5 rounded-md bg-[#2a2b33] p-2 ">
         <input
           type="text"
           value={message}
           onKeyDown={handleKeyDown}
           onChange={(e) => setMessage(e.target.value)}
-          className="flex-1 rounded-md bg-transparent p-5 focus:border-none focus:outline-none"
+          className="flex-1 rounded-md bg-transparent p-2 sm:p-3  text-sm sm:text-base focus:border-none focus:outline-none"
           placeholder="Enter message"
         />
         <button
           onClick={attachmentClickHandler}
-          className="text-neutral-500 transition-all duration-300 focus:border-none focus:text-white focus:outline-none"
+          className="text-neutral-500 transition-all duration-300 focus:border-none focus:text-white focus:outline-none p-1 sm:p-2"
         >
-          <GrAttachment className="text-2xl" />
+          <GrAttachment className="text-lg sm:text-xl md:text-2xl" />
         </button>
         <input
           type="file"
@@ -138,11 +141,11 @@ const MessageBar = () => {
         <div className="relative">
           <button
             onClick={() => setEmojiPickerOpen(true)}
-            className="text-neutral-500 transition-all duration-300 focus:border-none focus:text-white focus:outline-none"
+            className="text-neutral-500 transition-all duration-300 focus:border-none focus:text-white focus:outline-none p-1 sm:p-2"
           >
-            <RiEmojiStickerLine className="text-2xl" />
+            <RiEmojiStickerLine className="text-lg sm:text-xl md:text-2xl" />
           </button>
-          <div ref={emojiRef} className="absolute bottom-16 right-0">
+          <div ref={emojiRef} className="absolute bottom-12 sm:bottom-14 md:bottom-16 right-0 z-10">
             <EmojiPicker
               theme={Theme.DARK}
               open={emojiPickerOpen}
@@ -154,9 +157,9 @@ const MessageBar = () => {
       </div>
       <button
         onClick={handleSendMessage}
-        className="item-center flex justify-center rounded-md bg-[#8417ff] p-5 transition-all duration-300 hover:bg-[#741bda] focus:border-none focus:bg-[#741bda] focus:text-white focus:outline-none"
+        className="flex items-center justify-center rounded-md bg-[#8417ff] p-3 sm:p-4 md:p-5 transition-all duration-300 hover:bg-[#741bda] focus:border-none focus:bg-[#741bda] focus:text-white focus:outline-none"
       >
-        <IoSend className="text-2xl" />
+        <IoSend className="text-lg sm:text-xl md:text-2xl" />
       </button>
     </div>
   );
